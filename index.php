525c4dfcddb6e54b4467dfd4cbc10da35e4010f5
Saud Iqbal <6971514+saudiqbal@users.noreply.github.co>
2026-01-13 22:29:10 -0500

    Shorten Units

--- index.php
+++ index.php
@@ -192,7 +192,19 @@ x: {
 stacked: true,
 },
 y: {
-stacked: true
+stacked: true,
+ticks: {
+callback: function(value, index, ticks) {
+	function formatBytes(bytes) {
+		if (bytes === 0) return '0 Bytes';
+		const k = 1000;
+		const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+		const i = Math.floor(Math.log(bytes) / Math.log(k));
+		return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+	}
+	return formatBytes(value);
+	}
+}
 }
 },
 responsive: true
@@ -250,7 +262,19 @@ x: {
 stacked: true,
 },
 y: {
-stacked: true
+stacked: true,
+ticks: {
+callback: function(value, index, ticks) {
+	function formatBytes(bytes) {
+		if (bytes === 0) return '0 Bytes';
+		const k = 1000;
+		const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+		const i = Math.floor(Math.log(bytes) / Math.log(k));
+		return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+	}
+	return formatBytes(value);
+	}
+}
 }
 },
 responsive: true
@@ -308,7 +332,19 @@ x: {
 stacked: true,
 },
 y: {
-stacked: true
+stacked: true,
+ticks: {
+callback: function(value, index, ticks) {
+	function formatBytes(bytes) {
+		if (bytes === 0) return '0 Bytes';
+		const k = 1000;
+		const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+		const i = Math.floor(Math.log(bytes) / Math.log(k));
+		return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+	}
+	return formatBytes(value);
+	}
+}
 }
 },
 responsive: true
@@ -366,7 +402,19 @@ x: {
 stacked: true,
 },
 y: {
-stacked: true
+stacked: true,
+ticks: {
+callback: function(value, index, ticks) {
+	function formatBytes(bytes) {
+		if (bytes === 0) return '0 Bytes';
+		const k = 1000;
+		const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+		const i = Math.floor(Math.log(bytes) / Math.log(k));
+		return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+	}
+	return formatBytes(value);
+	}
+}
 }
 },
 responsive: true
