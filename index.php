8efe3239b11a1ed619742f6d75e8a57eb395fc91
Saud Iqbal <6971514+saudiqbal@users.noreply.github.co>
2026-01-22 06:20:09 -0500

    Quick fix

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 $startTime = array_sum(explode(' ', microtime()));
+header("Cache-Control: no-store, must-revalidate");
 include 'config.php';
 [$CurrentY, $CurrentM, $CurrentD, $CurrentH] = explode('-', date('Y-m-d-H'));
 $months = array(
